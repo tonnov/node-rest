c4f3e9415177dddf9f8d1140d6422aa246f0889c
tonov <nogueda@gmail.com>
2019-01-15 01:34:16 +0000

    add login

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,16 @@ process.env.PORT = process.env.PORT || 3000;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+
+// ==== Vencimiento de Token ====
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// ==== SEED autenticación =====
+
+process.env.SEED = process.env.SEED || 'seed-de-desarrollo';
+
+
 // ==== Base de datos ======
 
 let urlDB;
