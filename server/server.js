da1e2da980684456252453887b61825f86e6ef80
tonov <nogueda@gmail.com>
2019-01-21 23:59:13 +0000

    public folder

--- server/server.js
+++ server/server.js
@@ -2,13 +2,22 @@ require('./config/config.js');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 const bodyParser = require('body-parser');
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+
+// Parse application/json
+app.use(bodyParser.json());
+
+// Habilitar la carpeta public
+app.use( express.static( path.resolve(__dirname, '../public') ) );
+
+console.log(path.resolve(__dirname, '../public') );
 
 app.use(  require('./routes/index')  );
 
